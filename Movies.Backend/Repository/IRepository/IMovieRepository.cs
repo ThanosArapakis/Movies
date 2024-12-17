7bf293906e0b2b126b98c49c828c9d3a43d8cef2
Thanos Arapakis <aarapakis@01solutions.gr>
2024-12-17 16:16:01 +0200

    update .gitignore

---
+++ Movies.Backend/Repository/IRepository/IMovieRepository.cs
@@ -0,0 +1,15 @@
+﻿using Movies.Backend.Models;
+using System.Linq.Expressions;
+
+namespace Movies.Backend.Repository.IRepository
+{
+    public interface IMovieRepository
+    {
+        IEnumerable<Movie> GetAllMovies(Expression<Func<Movie, bool>>? filter = null);
+        Movie? GetMovie(int? id);
+        void AddMovie(Movie entity);
+        void RemoveMovie(Movie entity);
+
+        void Save();
+    }
+}
---
+++ Movies.Backend/Repository/MovieRepository.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Movies.Backend.Models;
+using Movies.Backend.Repository.IRepository;
+using Movies.Data;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace Movies.Backend.Repository
+{
+    public class MovieRepository : IMovieRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public MovieRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public void AddMovie(Movie movie)
+        {
+            _db.Movies.Add(movie);
+        }
+
+        public Movie? GetMovie(int? id)
+        {
+            return _db.Movies.FirstOrDefault(m => m.Id == id);
+        }
+
+        public IEnumerable<Movie> GetAllMovies(Expression<Func<Movie, bool>>? filter = null)
+        {
+            List<Movie> movies = _db.Movies.Where(filter).ToList(); 
+            return movies;
+        }
+
+        public void RemoveMovie(Movie movie)
+        {
+            _db.Movies.Remove(movie);
+        }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+    }
+}
